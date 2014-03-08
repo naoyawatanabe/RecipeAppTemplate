dfa1bb28bae7d75a0338c12558e7d88c3088a840
Naoya Watanabe <hello@showandtell.jp>
2014-03-08 10:57:49 +0900

    commit

---
+++ CustomTable/RecipeTableViewController.m
@@ -0,0 +1,66 @@
+//
+//  CustomTableViewController.m
+//  CustomTable
+//
+//  Created by Simon on 7/12/13.
+//  Copyright (c) 2013 Appcoda. All rights reserved.
+//
+
+#import "RecipeTableViewController.h"
+#import "RecipeTableCell.h"
+
+@interface RecipeTableViewController ()
+
+@end
+
+@implementation RecipeTableViewController
+{
+    NSArray *recipeNames;
+    NSArray *recipeImages;
+    NSArray *recipePrepTimes;
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    recipeNames = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+    
+    recipeImages = [NSArray arrayWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg", @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
+    
+    recipePrepTimes = [NSArray arrayWithObjects:@"30 min", @"30 min", @"20 min", @"30 min", @"10 min", @"1 hour", @"45 min", @"5 min", @"30 min", @"8 min", @"20 min", @"20 min", @"5 min", @"1.5 hour", @"4 hours", @"10 min", nil];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipeNames count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"CustomTableCell";
+    RecipeTableCell *cell = (RecipeTableCell *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    // Configure the cell...
+    if (cell == nil) {
+        cell = [[RecipeTableCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    // Display recipe in the table cell
+    cell.nameLabel.text = [recipeNames objectAtIndex:indexPath.row];
+    cell.thumbnailImageView.image = [UIImage imageNamed:[recipeImages objectAtIndex:indexPath.row]];
+    cell.prepTimeLabel.text = [recipePrepTimes objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+
+@end
